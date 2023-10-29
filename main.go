7175eb045f88338f6bbfe318e72e1c14c59439e8
ftambara <98750668+ftambara@users.noreply.github.com>
2023-10-29 19:41:30 -0300

    Register sample handlers

--- main.go
+++ main.go
@@ -9,9 +9,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World!"))
 }
 
+func noteView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Showing a note"))
+}
+
+func noteCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Creating a note... (don't wait for it)"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/notes/view/", noteView)
+	mux.HandleFunc("/notes/create/", noteCreate)
 
 	port := ":4000"
 	log.Printf("Starting server on %v", port)
