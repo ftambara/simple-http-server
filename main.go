66ead7501a18ee88b707a3146cbbd9b4eed47dd1
ftambara <98750668+ftambara@users.noreply.github.com>
2023-10-29 19:33:39 -0300

    Pass main.go through go fmt

--- main.go
+++ main.go
@@ -1,23 +1,22 @@
 package main
 
 import (
-    "net/http"
-    "log"
+	"log"
+	"net/http"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello World!"))
+	w.Write([]byte("Hello World!"))
 }
 
-func main(){
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", home)
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
 
-    port := ":4000"
-    log.Printf("Starting server on %v", port)
-    err := http.ListenAndServe(port, mux)
-    if err != nil {
-        log.Fatal(err)
-    }
+	port := ":4000"
+	log.Printf("Starting server on %v", port)
+	err := http.ListenAndServe(port, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
-
