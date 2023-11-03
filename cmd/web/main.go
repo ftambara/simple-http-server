6845ad4d4d8c8801cc237fee66f8df6958808434
ftambara <98750668+ftambara@users.noreply.github.com>
2023-11-03 07:53:51 -0300

    Sanitize FSServer

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(sanitizedFS{http.Dir("./ui/static/")})
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("/", home)
@@ -21,4 +22,35 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+type sanitizedFS struct {
+	dir http.Dir
+}
+
+// Prevent serving of static files directly
+func (sfs sanitizedFS) Open(path string) (http.File, error) {
+	f, err := sfs.dir.Open(path)
+	// Whatever was asked could truly not be found
+	if err != nil {
+		return nil, err
+	}
+	// If it's a directory, check if it has an index.html
+	s, err := f.Stat()
+	if err != nil {
+		return nil, err
+	} else if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err = sfs.dir.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return f, nil
+
 }
