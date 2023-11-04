11d0aa6b4f195abb39ace8e2e1bfbe04c8128048
ftambara <98750668+ftambara@users.noreply.github.com>
2023-11-04 11:02:27 -0300

    Add debug stacktrace

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"net/http"
+	"runtime/debug"
 )
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	app.errorLog.Print(err.Error())
+	app.errorLog.Output(2, string(debug.Stack()))
 	status := http.StatusInternalServerError
 	http.Error(w, http.StatusText(status), status)
 }
