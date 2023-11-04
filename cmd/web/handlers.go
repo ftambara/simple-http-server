7b53f193da3b206a2d5ab50748ba699d770fbb78
ftambara <98750668+ftambara@users.noreply.github.com>
2023-11-04 10:47:09 -0300

    Use a helper function for client errors

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,7 +9,7 @@ import (
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.clientError(w, http.StatusNotFound)
 		return
 	}
 	files := []string{
@@ -33,7 +33,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 func (app *application) noteView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 0 {
-		http.NotFound(w, r)
+		app.clientError(w, http.StatusNotFound)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
@@ -43,7 +43,7 @@ func (app *application) noteView(w http.ResponseWriter, r *http.Request) {
 func (app *application) noteCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write([]byte("Creating a note... (don't wait for it)"))
--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -9,3 +9,7 @@ func (app *application) serverError(w http.ResponseWriter, err error) {
 	status := http.StatusInternalServerError
 	http.Error(w, http.StatusText(status), status)
 }
+
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
